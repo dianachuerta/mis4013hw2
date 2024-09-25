62a3a4def3b24d81566bc117777b25db84d26291
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-09-25 13:30:58 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -3,17 +3,17 @@ $pageTitle = "Post result";
 include "view-header.php";
 ?>
     <h1>Post result</h1>
-   <?php
+
+<?php
 echo getDisplay();
 
 include "view-footer.php";
 
-function getDsiplay() {
+function getDisplay() {
     if (isset($_POST['my-name'])) {
-  return "<p>The value sent is: </p>"
-   . $_POST['my-name'];
-} else { 
-       return 
-  "<p>Nothing posted to the page.</p>";
-}}
+  return "<p>The value sent is:</p>" . $_POST['my-name'];
+} else {
+return  "<p>Nothing posted to the page</p>";
+}
+}
 ?>
